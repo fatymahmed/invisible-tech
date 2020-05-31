05ef16ecf7ef7d8c6cb1ff4e76023cec2b297a03
fatymahmed <fatima.ahmed.muhsin@gmail.com>
2020-05-31 20:17:04 +0300

    Format code according to the guidelines

--- index.js
+++ index.js
@@ -1,55 +1,55 @@
 const zipCodeAPI =
-  "https://api.zip-codes.com/ZipCodesAPI.svc/1.0/QuickGetZipCodeDetails/";
-const weatherAPI = "https://api.openweathermap.org/data/2.5/weather";
+  "https://api.zip-codes.com/ZipCodesAPI.svc/1.0/QuickGetZipCodeDetails/"
+const weatherAPI = "https://api.openweathermap.org/data/2.5/weather"
 
 function main(locations) {
   locations.forEach((location) => {
-    findWeatherForLocation(location);
-  });
+    findWeatherForLocation(location)
+  })
 }
 
 const findWeatherForLocation = async (location) => {
   if (typeof location === "number") {
-    const city = await getCityName(location);
+    const city = await getCityName(location)
     if (city) {
-      getWeatherAndTime(city);
+      getWeatherAndTime(city)
     }
   } else {
-    getWeatherAndTime(location);
+    getWeatherAndTime(location)
   }
-};
+}
 
 const getCityName = async (postalCode) => {
   try {
     const response = await fetch(
       `${zipCodeAPI}${postalCode}?key=3U7NI37W63J526BVXBS1`
-    );
-    const result = await response.json();
-    let city = result.City;
-    return city.replace(/ /g, "+");
+    )
+    const result = await response.json()
+    const city = result.City
+    return city.replace(/ /g, "+")
   } catch (err) {
-    alert(postalCode + " not found");
+    alert(postalCode + " not found")
   }
-};
+}
 
 const getWeatherAndTime = async (city) => {
   try {
     const response = await fetch(
       `${weatherAPI}?q=${city}&units=metric&APPID=e35c92eefa655c7d2e50f8101fc984ca`
-    );
-    const result = await response.json();
-    const weatherDescription = result.weather[0].description;
-    const temperature = result.main.temp;
-    const UTCtimeOffsetInHours = result.timezone / 3600;
-    const currentUserDate = new Date();
+    )
+    const result = await response.json()
+    const weatherDescription = result.weather[0].description
+    const temperature = result.main.temp
+    const UTCtimeOffsetInHours = result.timezone / 3600
+    const currentUserDate = new Date()
     const currentUTCTime =
-      currentUserDate.getTime() + currentUserDate.getTimezoneOffset() * 60000;
+      currentUserDate.getTime() + currentUserDate.getTimezoneOffset() * 60000
 
     const currentDateAndTime = new Date(
       currentUTCTime + 3600000 * UTCtimeOffsetInHours
-    );
+    )
 
-    city = city.replace(/\+/g, " ");
+    city = city.replace(/\+/g, " ")
 
     console.log(
       "The weather in " +
@@ -59,16 +59,16 @@ const getWeatherAndTime = async (city) => {
         " with a temperature of " +
         temperature +
         " degrees celcius"
-    );
+    )
     console.log(
       "The local date and time in " +
         city +
         " is " +
         currentDateAndTime.toLocaleString()
-    );
+    )
   } catch (error) {
-    alert(city + " not found");
+    alert(city + " not found")
   }
-};
+}
 
-main([10005, "mombasga", "turin"]);
+main([10005, "mombasga", "turin"])
