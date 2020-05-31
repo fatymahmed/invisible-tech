2fa311afd4d5ac6dc17c9f85080be37e1479eaa5
fatymahmed <fatima.ahmed.muhsin@gmail.com>
2020-05-31 12:46:07 +0300

    Add main method which finds weather for each location

--- index.js
+++ index.js
@@ -2,4 +2,21 @@ const zipCodeAPI =
   "https://api.zip-codes.com/ZipCodesAPI.svc/1.0/QuickGetZipCodeDetails/";
 const weatherAPI = "https://api.openweathermap.org/data/2.5/weather";
 
+function main(locations) {
+  locations.forEach((location) => {
+    findWeatherForLocation(location);
+  });
+}
+
+const findWeatherForLocation = async (location) => {
+  if (typeof location === "number") {
+    const city = await getCityName(location);
+    if (city) {
+      getWeatherAndTime(city);
+    }
+  } else {
+    getWeatherAndTime(location);
+  }
+};
+
 main([10005, "mombasga", "turin"]);
